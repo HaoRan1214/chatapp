5768c4d8e65a13d8ce3b26f7edf382fdee1ed6da
HaoRan1214 <haoran021214@icloud.com>
2024-05-25 00:46:50 +0800

    05250046

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,8 +1,7 @@
 // cypress/integration/sample_spec.js
 describe('My First Test', () => {
-    it('Visits the app and checks the title', () => {
-      cy.visit('http://localhost:3000');
-      cy.contains('Welcome');
-    });
+  it('Visits the app and checks the title', () => {
+    cy.visit('/');
+    cy.contains('Welcome');
   });
-  
+});
