383db1dc75f349d569d4f0c6df1acb1a632b283f
HaoRan1214 <haoran021214@icloud.com>
2024-05-24 23:43:44 +0800

    0524

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,28 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended', // 添加 TypeScript ESLint 插件
+  ],
+  parser: '@typescript-eslint/parser', // 使用 TypeScript 解析器
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-    ],
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 12,
-      sourceType: 'module',
-    },
-    plugins: [
-      'react',
-    ],
-    rules: {
-      'react/prop-types': 'off',
-    },
-  };
-  
+    ecmaVersion: 2021, // 支持最新的 ECMAScript 版本
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint', // 添加 TypeScript ESLint 插件
+  ],
+  rules: {
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-var-requires': 'off', // 示例规则，根据需要调整
+  },
+};
