e0d15e8179c22d96d6ada74e3937f0ff3bdb7b11
HaoRan1214 <haoran021214@icloud.com>
2024-05-25 10:57:19 +0800

    05251057

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -1,9 +1,11 @@
-// cypress.config.js
-
+// cypress.config.cjs
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
     baseUrl: 'http://localhost:3000',
   },
 });
