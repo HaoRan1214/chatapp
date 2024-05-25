0a78b4ca78c36b7d14542908122828c60a6ce023
HaoRan1214 <haoran021214@icloud.com>
2024-05-25 11:25:52 +0800

    05251125

--- cypress/e2e/sample.cy.js
+++ cypress/e2e/sample.cy.js
@@ -1,8 +1,7 @@
 // cypress/e2e/sample.cy.js
 describe('My First Test', () => {
-    it('Visits the app and checks the title', () => {
-      cy.visit('/');
-      cy.contains('Welcome'); // 修改为应用中实际存在的文本
-    });
+  it('Visits the app and checks the title', () => {
+    cy.visit('/');
+    cy.contains('Login'); // 确保这里的文本存在于页面上
   });
-  
+});
